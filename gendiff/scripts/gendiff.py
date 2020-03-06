7e494700f79746a7201de68b554f49041b280faa
Roman Kornveyts <kornveyts@gmail.com>
2020-03-06 10:35:27 +0200

    have finished step 2

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', metavar='FORMAT', help='set format of output')
+
+    args = parser.parse_args()
+    print(args.accumulate(args.integers))
+
+
+if __name__ == '__main__':
+    main()
---
+++ gendiff/scripts/script.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def main():
+    print('Hello, world!')
+    print('This is a python-script!')
+
+
+if __name__ == '__main__':
+    main()
