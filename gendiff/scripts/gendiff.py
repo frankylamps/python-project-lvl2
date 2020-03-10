300c45c1e2fe78f5011a8f20865cba1c983d9f23
Roman Kornveyts <kornveyts@gmail.com>
2020-03-10 17:51:31 +0200

    step three is almost ready

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
+from gendiff.scripts.gendiff_tool import generate_diff
 
 
 def main():
     parser = argparse.ArgumentParser(description='Generate diff')
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
     parser.add_argument('-f', '--format', metavar='FORMAT', help='set format of output')
 
     args = parser.parse_args()
-    print(args.accumulate(args.integers))
-
+    d1 = json.load(open(args.first_file))
+    d2 = json.load(open(args.second_file))
+    print(generate_diff(d1, d2))
 
 if __name__ == '__main__':
     main()
+
--- gendiff/scripts/script.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-def main():
-    print('Hello, world!')
-    print('This is a python-script!')
-
-
-if __name__ == '__main__':
-    main()
