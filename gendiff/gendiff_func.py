9fc6c9e3e2967c15055145d1e5e2a1943897a6fe
Roman Kornveyts <kornveyts@gmail.com>
2020-03-11 19:40:23 +0200

    have added gendiff func

--- gendiff/gendiff_func.py
+++ gendiff/gendiff_func.py
@@ -5,8 +5,7 @@ from gendiff.tools.gendiff_tool import generate_diff
 
 
 def gendiff_func(path1, path2):
-    d1 = json.load(open(path1))
-    d2 = json.load(open(path2))
+    """Compare two json files and get the difference."""  # noqa:  DAR101
+    d1 = json.load(open(path1))  # noqa: WPS515
+    d2 = json.load(open(path2))  # noqa: WPS515
     print(generate_diff(d1, d2))
-
-
