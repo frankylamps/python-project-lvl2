7b9320a50012666ed72151d81a97c3424ecb1f02
Roman Kornveyts <kornveyts@gmail.com>
2020-12-19 22:10:39 +0200

    deleted lint exclusion from makefile

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ install:
 	poetry install
 
 lint:
-	poetry run flake8 --per-file-ignores="__init__.py:F401"
+	poetry run flake8
 
 tests:
 	poetry run pytest --cov=gendiff --cov-report xml
--- gendiff/format/__init__.py
+++ gendiff/format/__init__.py
@@ -1,6 +1,6 @@
-from gendiff.format.json import format as json
-from gendiff.format.plain import format as plain
-from gendiff.format.default import format as default
+from gendiff.format.json import format as json  # noqa: F401
+from gendiff.format.plain import format as plain  # noqa: F401
+from gendiff.format.default import format as default  # noqa: F401
 
 
 FORMATTERS = (JSON, PLAIN, DEFAULT) = (
