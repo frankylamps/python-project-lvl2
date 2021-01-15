166c15abe1a7142a1ea3a84e71b84d8599a581e8
Roman Kornveyts <kornveyts@gmail.com>
2021-01-15 07:28:02 +0200

    simplified gendiff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -12,20 +12,20 @@ def load_file(path):
 
 def gendiff_inner(dict_old, dict_new):
     diff = {}
-    for key in set(dict_old.keys()) & set(dict_new.keys()):
-        value_old, value_new = dict_old[key], dict_new[key]
-        if value_old == value_new:
-            diff[key] = ('unchanged', value_old)
-        elif isinstance(value_old, dict) and isinstance(value_new, dict):
-            diff[key] = ('nested', gendiff_inner(value_old, value_new))
-        else:
-            diff[key] = ('changed', (dict_old.get(key), dict_new.get(key)))
-    removed_keys = set(dict_old.keys()) - set(dict_new.keys())
-    added_keys = set(dict_new.keys()) - set(dict_old.keys())
+    added_values = set(dict_new.keys()) - set(dict_old.keys())
+    removed_values = set(dict_old.keys()) - set(dict_new.keys())
     for key in set(dict_old.keys()) | set(dict_new.keys()):
-        if key in removed_keys:
+        value_old, value_new = dict_old.get(key), dict_new.get(key)
+        if value_old and value_new:
+            if value_old == value_new:
+                diff[key] = ('unchanged', value_old)
+            elif isinstance(value_old, dict) and isinstance(value_new, dict):
+                diff[key] = ('nested', gendiff_inner(value_old, value_new))
+            elif value_old != value_new:
+                diff[key] = ('changed', (dict_old.get(key), dict_new.get(key)))
+        elif key in removed_values:
             diff[key] = ('removed', dict_old[key])
-        if key in added_keys:
+        elif key in added_values:
             diff[key] = ('added', dict_new[key])
     return diff
 
